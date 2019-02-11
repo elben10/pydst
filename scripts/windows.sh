96c904b22a20da388fa69cfb00c580b88d87ce91
Jakob Jul Elben <elbenjakobjul@gmail.com>
2019-02-11 09:53:49 +0100

    Update windows.sh

--- scripts/windows.sh
+++ scripts/windows.sh
@@ -1,7 +1,2 @@
-if [ "${PYTHON_VERSION}" == "3.8-dev" ]
-then
-  choco install python --pre
-else
-  choco install python --version ${PYTHON_VERSION}
-fi
+choco install python --version ${PYTHON_VERSION}
 export PATH="/c/python${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1}/Scripts:/c/python${PYTHON_VERSION:0:1}${PYTHON_VERSION:2:1}:${PATH}"
